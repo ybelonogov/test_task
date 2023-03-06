e3a0e93fe14112db5667cc4ab10c2f5c58704570
ybelonogov <aleshka59000@gmail.com>
2023-03-06 21:21:22 +0300

    go to dp

--- app/src/main/java/com/example/vk_test_task/ClockView.kt
+++ app/src/main/java/com/example/vk_test_task/ClockView.kt
@@ -2,6 +2,7 @@ package com.example.vk_test_task
 
 import android.content.Context
 import android.content.res.Configuration
+import android.content.res.Resources
 import android.graphics.*
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
@@ -70,6 +71,8 @@ class ClockView @JvmOverloads constructor(
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         val size = min(measuredWidth, measuredHeight)
+        layoutParams.height=min(layoutParams.height,layoutParams.width)
+        layoutParams.width=min(layoutParams.height,layoutParams.width)
         setMeasuredDimension(size, size)
     }
 
@@ -106,21 +109,25 @@ class ClockView @JvmOverloads constructor(
     }
 
     private val paint = Paint()
+    val Int.dp: Int
+        get() = (this / Resources.getSystem().displayMetrics.density).toInt()
+    val Int.px: Int
+        get() = (this * Resources.getSystem().displayMetrics.density).toInt()
     private fun drawClockFace(canvas: Canvas?) {
         paint.color = Color.GREEN
 
         val bitmap = (_clockFace as BitmapDrawable).bitmap
-        val radius = min(width, height) / 2f
-        canvas?.drawCircle(width / 2f, height / 2f, radius, paint)
+        val radius = min(layoutParams.width, layoutParams.height) / 2f
+        canvas?.drawCircle(layoutParams.width / 2f, layoutParams.height / 2f, radius, paint)
         val scaledBitmap =
             Bitmap.createScaledBitmap(bitmap, radius.toInt() * 2, radius.toInt() * 2, false)
         val shader = BitmapShader(scaledBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
         paint.shader = shader
-        canvas?.drawCircle(width / 2f, height / 2f, radius, paint)
+        canvas?.drawCircle(layoutParams.width / 2f, layoutParams.height / 2f, radius, paint)
     }
 
     private fun drawSecondArrow(canvas: Canvas?, centerX: Float, centerY: Float) {
-        val secondHandLength = centerX - height * 0.05f
+        val secondHandLength = centerX - layoutParams.height * 0.05f
         val secondHandAngle = second * 6f - 90f.toDouble()
         val secondHandX = centerX + cos(Math.toRadians(secondHandAngle)) * secondHandLength
         val secondHandY = centerY + sin(Math.toRadians(secondHandAngle)) * secondHandLength
@@ -135,7 +142,7 @@ class ClockView @JvmOverloads constructor(
     }
 
     private fun drawMinuteArrow(canvas: Canvas?, centerX: Float, centerY: Float) {
-        val minuteHandLength = centerX - height * 0.1f
+        val minuteHandLength = centerX - layoutParams.height * 0.1f
         val minuteHandAngle = (minute + second / 60f) * 6f - 90f.toDouble()
         val minuteHandX = centerX + cos(Math.toRadians(minuteHandAngle)) * minuteHandLength
         val minuteHandY = centerY + sin(Math.toRadians(minuteHandAngle)) * minuteHandLength
@@ -150,7 +157,7 @@ class ClockView @JvmOverloads constructor(
     }
 
     private fun drawHourArrow(canvas: Canvas?, centerX: Float, centerY: Float) {
-        val hourHandLength = centerX - height * 0.2f
+        val hourHandLength = centerX - layoutParams.height * 0.2f
         val hourHandAngle = (hour + minute / 60f) * 30f - 90f.toDouble()
         val hourHandX = centerX + cos(Math.toRadians(hourHandAngle)) * hourHandLength
         val hourHandY = centerY + sin(Math.toRadians(hourHandAngle)) * hourHandLength
@@ -176,8 +183,8 @@ class ClockView @JvmOverloads constructor(
             LocalDateTime.now().minute,
             LocalDateTime.now().second
         )
-        val centerX = width / 2f
-        val centerY = height / 2f
+        val centerX = layoutParams.width / 2f
+        val centerY = layoutParams.height / 2f
         drawSecondArrow(canvas, centerX, centerY)
         drawMinuteArrow(canvas, centerX, centerY)
         drawHourArrow(canvas, centerX, centerY)
